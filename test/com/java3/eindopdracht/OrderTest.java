26d57531fb1a2c051a18d934cffdc06177909176
ArjanHeijnen <33656909+ArjanHeijnen@users.noreply.github.com>
2020-01-14 12:09:30 +0100

    ohja dit ook

--- test/com/java3/eindopdracht/OrderTest.java
+++ test/com/java3/eindopdracht/OrderTest.java
@@ -24,38 +24,6 @@ class OrderTest {
         } catch (Exception e) {
             //yeet
         }
-       /* Shop MacEmmen = new Shop();
-
-        MacEmmen.addDiscountCode("10OFF", "€10");
-        MacEmmen.addDiscountCode("HALFTHEPRICE", "50%");
-        MacEmmen.printDiscountList();
-        oke
-
-        Order order1 = MacEmmen.createOrder();
-        MacEmmen.placeOrder(order1);
-
-        order1.addBurger("Big Tasty", 2);
-        order1.addBurger("Big Mac", 2);
-        order1.addDrink("Sinas", 4);
-        order1.addDiscountCode("HALFTHEPRICE");
-        order1.addDiscountCode("10OFF");
-        order1.printThisOrder();
-        order1.printAllergyInfoPerMenuItem();
-
-
-        Order order2 = MacEmmen.createOrder();
-        MacEmmen.placeOrder(order2);
-
-        order2.addBurger("XXL Meat Burger", 2);
-        order2.addDrink("Milkshake", 4);
-        order2.addDiscountCode("10OFF");
-        order2.printThisOrder();
-        order2.printAllergyInfoPerMenuItem();
-
-        //MacEmmen.printAllOrders();
-        MacEmmen.printRevenue();
-        */
-
     }
 
     @AfterEach
